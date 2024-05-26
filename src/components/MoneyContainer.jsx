1ca999ae389ae7c86948b963e438b0812c7f2fb4
hyeonw8 <whgusrud223@gmail.com>
2024-05-26 20:05:22 +0900

    🚚 List 폴더 생성 및 이동 후 경로 변경

--- src/components/MoneyContainer.jsx
+++ src/components/MoneyContainer.jsx
@@ -1,5 +1,5 @@
 import MoneyForm from '../components/MoneyForm';
-import MoneyList from './MoneyList';
+import MoneyList from './List/MoneyList';
 import styled from 'styled-components';
 import MonthList from './Calendar/MonthList';
 import { useEffect, useState } from 'react';
