271528c1b184558aa51a56450f30e7ec0911f33b
hyeonw8 <whgusrud223@gmail.com>
2024-05-26 18:10:05 +0900

    ♻️ Context API를 사용해 상태 관리 재구성
    MoneyContainer.jsx 리팩터링

--- src/components/MoneyContainer.jsx
+++ src/components/MoneyContainer.jsx
@@ -2,7 +2,8 @@ import MoneyForm from '../components/MoneyForm';
 import MoneyList from './MoneyList';
 import styled from 'styled-components';
 import MonthList from './Calendar/MonthList';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
+import { DataContext } from '../shared/DataContext';
 
 const StWrapper = styled.div`
   width: 100vw;
@@ -13,34 +14,9 @@ const StWrapper = styled.div`
   padding: 10px;
 `;
 
-const MoneyContainer = ({ data, setData }) => {
+const MoneyContainer = () => {
+  const { data, selectedMonth, setFilteredData } = useContext(DataContext);
   const monthArr = ['1','2','3','4','5','6','7','8','9','10','11','12',];
-  const savedMonth = JSON.parse(localStorage.getItem('month')) || [];
-
-  const [selectedMonth, setSelectedMonth] = useState(savedMonth);
-  const [activeIndex, setActiveIndex] = useState(
-    savedMonth.length === 0 ? null : savedMonth
-  );
-
-  // const filteredData = [...data].filter((fdata) => {
-  //   const spliceMonth = fdata.date.slice(5, 7).replace('0', '');
-  //   //console.log('>>> spliceMonth',Number(spliceMonth));
-  //   //console.log('>>>selectedMonth', selectedMonth)
-  //   return Number(spliceMonth) === selectedMonth; 
-  // });
-  // //const [filteredData, setfFilteredData] = useState(filterData);
-  
-  // const addData = (newData) => {
-  //   const updatedData = [...data, newData];
-  //   setData(updatedData);
-  //   setFilteredData(filterData(updatedData, selectedMonth));
-  // };
-  
-  // useEffect(() => {
-  //   console.log(filteredData);
-  // }, [filteredData]);
-
-  const [filteredData, setFilteredData] = useState([]);
 
   const filterData = (data, selectedMonth) => {
     return data.filter((fdata) => {
@@ -56,20 +32,12 @@ const MoneyContainer = ({ data, setData }) => {
   return (
     <StWrapper>
       <MoneyForm 
-        data={data} 
-        setData={setData} 
-        setFilteredData={setFilteredData} 
-        selectedMonth={selectedMonth} 
         filterData={filterData}
       />
       <MonthList
         monthArr={monthArr}
-        setSelectedMonth={setSelectedMonth}
-        selectedMonth={selectedMonth}
-        activeIndex={activeIndex}
-        setActiveIndex={setActiveIndex}
       />
-      <MoneyList data={data} setData={setData} filteredData={filteredData} />
+      <MoneyList />
     </StWrapper>
   );
 };
