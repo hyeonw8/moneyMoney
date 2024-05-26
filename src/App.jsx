6bd3c3aef768255b178923c279b2fefe0b44fd7b
hyeonw8 <whgusrud223@gmail.com>
2024-05-26 17:59:06 +0900

    ♻️ Context API를 사용해 상태 관리 재구성
    App.js 리팩터링

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from 'react';
+
 import './App.css';
 import Router from './shared/Router';
-import moneyData from './moneyData.json';
 import Globalstyle from './shared/Globalstyle';
+import { DataProvider } from './shared/DataContext';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  //console.log(data);
-  useEffect(() => {
-    setData(moneyData);
-  }, []);
-
+  
   return (
     <>
-      <Globalstyle />
-      <Router data={data} setData={setData} />
+      <DataProvider>
+        <Globalstyle />
+        <Router />
+      </DataProvider>
     </>
     
   );
