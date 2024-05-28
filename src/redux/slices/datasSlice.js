596fb70ae30d1fe4eec00ef05a298c53aaa6e69d
hyeonw8 <whgusrud223@gmail.com>
2024-05-28 17:49:30 +0900

    ⚰ 불필요한 코드 및 주석 제거

--- src/redux/slices/datasSlice.js
+++ src/redux/slices/datasSlice.js
@@ -1,36 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit"
 import moneyData from '/src/moneyData.json';
 
-// const savedMonth = JSON.parse(localStorage.getItem('month')) || [];
 const getSelectedMonth = () => {
   return JSON.parse(localStorage.getItem('month')) || []
 }
-// const getActiveIndex = () => {
-//   return savedMonth.length === 0 ? null : savedMonth;
-// }
 
 const initialState = {
   data: moneyData,
   selectedMonth: getSelectedMonth(),
-  // filteredData: [],
 }
 
 const datasSlice = createSlice({
   name: 'datas',
   initialState,
   reducers: {
-    // setData: (state, action) => {
-    //   state.data = action.payload;
-    // },
     setSelectedMonth: (state, action) => {
       state.selectedMonth = action.payload;
       localStorage.setItem('month', JSON.stringify(action.payload));
     },
-    // setFilteredData: (state, action) => {
-    //   state.filteredData = action.payload;
-    // },
     addData: (state, action) => {
-      // 받아오는 거 새로운 데이터(지출 내역)
       state.data.push(action.payload);
     },
     deleteData: (state, action) => {
