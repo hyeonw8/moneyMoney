864405618a914c55594b53dee0d765a0735c54bc
hyeonw8 <whgusrud223@gmail.com>
2024-05-30 16:31:09 +0900

    ♻️ Provider 위치 수정, App.jsx에서 main.jsx로 이동
    Provider를 App 컴포넌트에 사용할 경우, App 컴포넌트에서는 redux를 사용할 수 없음. 따라서 App 컴포넌트도 사용할 수 있도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css';
 import Router from './shared/Router';
 import Globalstyle from './shared/Globalstyle';
-import store from './redux/config/configStore';
-import { Provider } from 'react-redux';
 
 function App() {
 
   return (
     <>
-      <Provider store={store}>
-        <Globalstyle />
-        <Router />
-      </Provider>
+      <Globalstyle />
+      <Router />
     </>
   );
 }
